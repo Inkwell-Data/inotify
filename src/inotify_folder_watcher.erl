a71c3f5237ddc42d3170beafc199d0023421d8ba
Massimo Cesaro <massimo.cesaro@gmail.com>
2024-05-08 16:59:43 +0200

    monitor all events - report create, delete and move

--- src/inotify_folder_watcher.erl
+++ src/inotify_folder_watcher.erl
@@ -3,8 +3,38 @@
 
 -compile(export_all).
 
+
+%% /* the following are legal, implemented events that user-space can watch for */
+%% #define IN_ACCESS           0x00000001  /* File was accessed */
+%% #define IN_MODIFY           0x00000002  /* File was modified */
+%% #define IN_ATTRIB           0x00000004  /* Metadata changed */
+%% #define IN_CLOSE_WRITE      0x00000008  /* Writtable file was closed */
+%% #define IN_CLOSE_NOWRITE    0x00000010  /* Unwrittable file closed */
+%% #define IN_OPEN             0x00000020  /* File was opened */
+%% #define IN_MOVED_FROM       0x00000040  /* File was moved from X */
+%% #define IN_MOVED_TO         0x00000080  /* File was moved to Y */
+%% #define IN_CREATE           0x00000100  /* Subfile was created */
+%% #define IN_DELETE           0x00000200  /* Subfile was deleted */
+%% #define IN_DELETE_SELF      0x00000400  /* Self was deleted */
+%% 
+%% /* the following are legal events.  they are sent as needed to any watch */
+%% #define IN_UNMOUNT          0x00002000  /* Backing fs was unmounted */
+%% #define IN_Q_OVERFLOW       0x00004000  /* Event queued overflowed */
+%% #define IN_IGNORED          0x00008000  /* File was ignored */
+%% 
+%% /* helper events */
+%% #define IN_CLOSE            (IN_CLOSE_WRITE | IN_CLOSE_NOWRITE) /* close */
+%% #define IN_MOVE             (IN_MOVED_FROM | IN_MOVED_TO) /* moves */
+%% 
+%% /* special flags */
+%% #define IN_ISDIR            0x40000000  /* event occurred against dir */
+%% #define IN_ONESHOT          0x80000000  /* only send event once */
+
+
 -define(POLL_DELAY, 10).
--define(MASK, 16#4000410A). %ISDIR, CREATE, CLOSE_WRITE, Q_OVERFLOW, MODIFY
+%% -define(MASK, 16#4000410A). %ISDIR, CREATE, CLOSE_WRITE, Q_OVERFLOW, MODIFY
+%% -define(MASK, 16#4000430A). %ISDIR, CREATE, CLOSE_WRITE, Q_OVERFLOW, MODIFY, DELETE
+-define(MASK, 0). %% All events
 
 watch_file(Pid, Path) -> Pid ! {watch_file, Path}.
 watch_folders(Pid, Folders) -> Pid ! {watch_folders, Folders}.
@@ -89,10 +119,12 @@ handle_info(tick, S) ->
                     case E of
                         {inotify, invalid_event} -> pass;
                         {inotify, Wd, Mask, Cookie, Filename} ->
+                            %% io:format("[~p] ~p inotify Mask ~p, File ~p~n",[?MODULE, ?FUNCTION_NAME, Mask, Filename]), 
                             IsWritten = lists:member(close_write, Mask),
                             IsDir = lists:member(isdir, Mask),
                             Create = lists:member(create, Mask),
-                            Delete = lists:member:(delete, Mask),
+                            Delete = lists:member(delete, Mask),
+                            Moved = lists:member(moved_from, Mask),
 
                             %File written to and changed
                             case {IsDir, IsWritten} of
@@ -107,6 +139,14 @@ handle_info(tick, S) ->
                                 {false, true} -> 
                                     AbsName3 = wd_lookup_absname(WdLookup, Wd, Filename),
                                     Parent ! {inotify, delete, AbsName3};
+                                _ -> pass
+                            end,
+
+                            % File moved
+                            case {IsDir, Moved} of
+                                {false, true} -> 
+                                    AbsName4 = wd_lookup_absname(WdLookup, Wd, Filename),
+                                    Parent ! {inotify, moved, AbsName4};
                                 _ -> pass
                             end,
 
